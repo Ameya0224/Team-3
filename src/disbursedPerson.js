ac3765baf70e41e6619ed104edb1730344565eeb
Ameya0224 <ameya.brahmankar@eduvanz.com>
2020-12-08 11:03:10 +0530

    Accordion initialized

--- src/disbursedPerson.js
+++ src/disbursedPerson.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import styles from './App.css'
+import {Card, Button, Accordion} from "react-bootstrap";
+
+
+
 
 function Person({person}) {
 
     return (
         <div>
-            <table class="table">
+            
+                            
+<Accordion defaultActiveKey="1">
+<table class="table">
               <tbody>
 
                 <tr>
@@ -22,20 +29,64 @@ function Person({person}) {
                             <td width="15%">Rs.{person.next_pay_amt}</td>
                             <td width="5%"><span class="badge dis-badge badge-pill badge-info">{person.status}</span></td>
                             <td width="10%"><button type="button" class="btn repay-btn btn-outline-primary">{person.repay}</button></td>
-                            <td></td>
+                            <td width></td>
+                            
+  
+      <Accordion.Toggle as={Button} variant="link" eventKey="0">
+      <img border="0"  src="https://cdn4.iconfinder.com/data/icons/podcast-collection/100/caret-512.png"  width="25" height="25"></img>
+      </Accordion.Toggle>
+      </tr>
+
+</tbody>
+
+</table>
+{/* </div> */}
+</div>
+</td>
+</tr>
+</tbody>
+
+</table>
+    <Accordion.Collapse eventKey="0">
+    <div className={styles.cardy} class="card">
+                      {/* <div class="card"> */}
+                      <table class="table">
+                        <tbody>
+                          <div><p>Loan Details</p></div>
+                          <tr class="">
+
+                            <td align="center">{person.name}</td>
+                            <td align="left">{person.inst_name}</td>
+                            <td align="left">Rs.{person.course_amt}</td>
+                            <td width="20%">Rs. {person.outstanding}/Rs. {person.loan}</td>
+                            <td width="10%">{person.paid_emi}/{person.total_emi}</td>
 
                           </tr>
 
+                          <tr class="">
+
+                            <td align="center">Name of borrower</td>
+                            <td align="left">Name of Institute</td>
+                            <td align="left">Course Amount (Rs.)</td>
+                            <td width="30%">Outstanding Amount / Total Amount</td>
+                            <td width="10%">Paid / Total EMIs</td>
+
+                          </tr>
+ 
                         </tbody>
 
                       </table>
                       {/* </div> */}
                     </div>
-                  </td>
-                </tr>
-              </tbody>
+    </Accordion.Collapse>
+    
 
-            </table>
+  
+</Accordion>
+
+                            
+
+                  
           </div>
 
     )
